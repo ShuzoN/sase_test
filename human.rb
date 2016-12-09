452a53a78722760173ffd9bc6cc3ff884c2ad645
shuzon <d.namu.r21@gmail.com>
2016-12-09 19:55:57 +0900

    mod: completed human class

--- human.rb
+++ human.rb
@@ -8,4 +8,24 @@ class Human
     @height=30 #cm
     @weight=3  #kg
   end
+
+  def was_named(n)
+    @name = n
+  end
+
+  def happy_birthday
+    @age += 1
+  end
+
+  def get_fat(w)
+    @weight+=w
+  end
+
+  def lost_weight(w)
+    @weight-=w
+  end
+
+  def grow(h)
+    @height+=h
+  end
 end
